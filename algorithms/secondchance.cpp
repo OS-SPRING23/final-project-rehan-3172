0ef6b066a9ca90b2429750790511071d923d48ae
Muhammad Rehan Khan <muhammadrehankhan000@gmail.com>
2023-05-13 19:04:14 +0500

    Testing

--- algorithms/secondchance.cpp
+++ algorithms/secondchance.cpp
@@ -1,56 +1,6 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-class SCD{
-public:
-    SCD();
-    bool executeSCD(vector<int>, int);
-    void setFrames(int);
-    int getHits();
-    int getFaults();
-private:
-    int frames,hits,faults;
-    // int hits;
-    // int
-
-};
-SCD::SCD(){
-    this->frames=3;//default
-    hits=0;
-    faults=0;
-}
-void SCD::setFrames(int frames){
-    this->frames=frames;
-}
-int SCD::getHits(){
-    return hits;
-}
-int SCD::getFaults(){
-    return faults;
-}
-
-bool SCD::executeSCD(vector<int>proc, int frames){
-    list exec(frames);
-    
-    for(int i=0;i<proc.size();i++){
-        int currProc=proc[i];
-        if(exec.search(currProc)){
-            // exec.deleteSpecific(currProc);
-            //exec.foundLRU(currProc);
-            // exec.deleteSpecific(currProc);
-            // exec.append(currProc);
-            hits++;
-            cout<<i<<" Proc: "<<currProc<<" Hits: "<<hits<<endl;
-        }
-        else{
-            //exec.append(currProc);
-            exec.search_v(proc,i,currProc);	//this will search for future pages and then order the list according to that.
-            faults++;
-            cout<<i<<" Proc: "<<currProc<<" Fault: "<<faults<<endl;
-        }
-    }
-}
-
 class node
 {
     public:
@@ -166,8 +116,65 @@ class list
         return false;
         }
 };
+class SCD{
+public:
+    SCD();
+    bool executeSCD(vector<int>, int);
+    void setFrames(int);
+    int getHits();
+    int getFaults();
+private:
+    int frames,hits,faults;
+    // int hits;
+    // int
+
+};
+SCD::SCD(){
+    this->frames=3;//default
+    hits=0;
+    faults=0;
+}
+void SCD::setFrames(int frames){
+    this->frames=frames;
+}
+int SCD::getHits(){
+    return hits;
+}
+int SCD::getFaults(){
+    return faults;
+}
+
+bool SCD::executeSCD(vector<int>proc, int frames){
+    list exec(frames);
+    
+    for(int i=0;i<proc.size();i++){
+        int currProc=proc[i];
+        if(exec.search(currProc)){
+            // exec.deleteSpecific(currProc);
+            //exec.foundLRU(currProc);
+            // exec.deleteSpecific(currProc);
+            // exec.append(currProc);
+            exec.update(currProc);
+            hits++;
+            cout<<i<<" Proc: "<<currProc<<" Hits: "<<hits<<endl;
+        }
+        else{
+            //exec.append(currProc);
+            //exec.search_v(proc,i,currProc);	//this will search for future pages and then order the list according to that.
+            exec.update(currProc);
+            faults++;
+            cout<<i<<" Proc: "<<currProc<<" Fault: "<<faults<<endl;
+        }
+    }
+}
+
+
 int main()
 {
-    
+    vector<int> proc={2, 5, 10, 1, 2 ,2 ,6, 9 ,1 ,2 ,10, 2, 6, 1, 2, 1 ,6 ,9, 5, 1};
+    //list l(3);
+    SCD test;
+    test.executeSCD(proc,3);
+
 
 }
