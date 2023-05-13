5bcf6ed35d8db511f71be982f1caf0fc305a5013
Muhammad Rehan Khan <muhammadrehankhan000@gmail.com>
2023-05-13 13:35:38 +0500

    Added Implementation Functions for Second Chance

--- algorithms/secondchance.cpp
+++ algorithms/secondchance.cpp
@@ -19,30 +19,85 @@ class list
     public:
    node *head;
    node *curr;
-   node *tail;
+  // node *tail;
     int len;
     int frames;
+    list(int frames)
+    {
+    	head=NULL;
+		//tail=NULL;
+        this->frames=frames;
+        len=0;
+    }
     void *append(int a)
     {
         node* temp= new node(a);
-			if(head==NULL){
-				head=temp;
-				tail=temp;
-                len++;
-			}
-			else{
-                if(len==frames){
-                   // deleteHead();
-                }
-				else{
-				len++;
-				}
-				tail->next=temp;
-				tail=temp;
-
-			}
+		if(head==NULL)
+        {
+			head=temp;
+			//tail=temp;
+            curr=head;
+            head->next=head;
+            len++;
 		}
+		else
+        {
+            if(len==frames)
+            {
+                   update(a);    
+            }
+			else
+            {
+				len++;
+			}
+			node*temp1=head;
+            while(temp1->next!=head)
+            {
+                temp1=temp1->next;
+            }
+            temp1->next=temp;
+            temp->next=head;
+            curr=temp;
+
+		}
+	}
+    void *update(int a)
+    {
+        int flag=0;
+        node *temp=head;
+        while(temp->next!=head)
+        {
+            temp=temp->next;
+            if(temp->data==a)
+            {
+                temp->chance=true;
+                flag=1;
+            }
+        }
+        if(flag==1)
+        {
+
+        }
+        // else if(curr->chance==true)
+        // {
+        //     curr->chance=false;
+        //     curr=curr->next;
+        // }
+        // else 
+        // {
+        //     curr->data=a;
+        // }
+        while(curr->chance==true)
+        {
+            curr->chance=false;
+            curr=curr->next;
+        }
+        curr->data=a;
+        curr=curr->next;
+
+        
     }
+    
 };
 int main()
 {
