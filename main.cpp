46970aa3bc92f1d773f6558bb5387d6814cde794
Auzek2002 <azaanukhan@gmail.com>
2023-05-13 11:59:09 -0700

    Formatting of main function done (little left)

--- main.cpp
+++ main.cpp
@@ -20,30 +20,22 @@ void runAll(vector<int>proc, int frames);
 
 
 int main(){
-    // string input;
-
-    // int frames, numOfProcesses;
-    int frames=3;
-    vector<int> processes={3 ,2 ,1 ,4 ,2 ,1 ,7 ,6 ,2 ,1 ,2 ,3 ,6 ,5 ,1, 2, 3 ,6 ,4 ,2};
-    
+    cout<<"\t\t*********************PAGE REPLACEMENT PROJECT***********************\n\n";
+    cout<<"Enter the number of Frames: ";
+    int frames;
+    cin>>frames;
+    vector<int> processes;
+    cout<<"Enter the data for the data stream (PRESS -1 TO STOP TAKING INPUT): ";
+    while(1){
+    	int n;
+    	cin>>n;
+    	if(n==-1){
+    		break;
+		}
+    	processes.push_back(n);
+	}
+	system("cls");
     runAll(processes, frames);
-    //code commented out for testing
-    // cout<<"Enter the number of processes: ";
-    // cin>>numOfProcesses;
-    // cout<<"Enter your processes: ";
-    // for(int i=0;i<numOfProcesses;i++){
-    //     int temp;
-    //     cin>>temp;
-    //     processes.push_back(temp);
-    // }
-    // cout<<"\nEnter the number of frames: ";
-    // cin>>frames;
-
-//    testFIFO();
-//    testLRU();
-	//   testOPT();
-    // testSCD();
-    // cout<<"Enter your proceses without spaces(12345): "
 }
 void runAll(vector<int>proc, int frames){
     cout<<"Page Replacement Algorithms\n";
@@ -68,6 +60,16 @@ void runAll(vector<int>proc, int frames){
     for(int i=0;i<4;i++){
         cout<<names[i]<<"   \t |"<<proc.size()-faults[i]<<"\t    |"<<faults[i]<<" \t|\n";
     }
+    
+    int min=faults[0];
+    int idx=0;
+    for(int i=0;i<4;i++){
+    	if(faults[i]<min){
+    		min=faults[i];
+    		idx=i;
+		}
+	}
+	cout<<"The most optimal algorithm for the entered data is: "<<names[idx]<<" with the number of page faults being: "<<faults[idx]<<endl;
     
 
 }
@@ -101,48 +103,48 @@ int runOPT(vector<int>proc, int frames){
     cout<<"OPT FAULTS: "<<opt.getFaults()<<endl<<endl;
     return opt.getFaults();
 }
-void testOPT(){
-	cout<<"OPT TEST\n";
-	// vector<int>p={7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1};
-    //frames=3
-
-	vector<int>p={7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2};
-
-	int frames=4;
-    OPT opt;
-    opt.executeOPT(p,frames);
-    cout<<"OPT HITS: "<<opt.getHits()<<endl;
-    cout<<"OPT FAULTS: "<<opt.getFaults()<<endl<<endl;
-}
-void testSCD(){
-    
-    vector<int> proc={2, 5, 10, 1, 2 ,2 ,6, 9 ,1 ,2 ,10, 2, 6, 1, 2, 1 ,6 ,9, 5, 1};
-    //vector<int> p1={2 ,5 ,10, 1 ,2 ,2, 6, 9, 1,2, 10, 2, 6, 1 ,2 ,1, 6 ,9 ,5 ,1};
-   vector<int> p={3, 2, 1, 4, 2, 1, 7, 6, 2, 1, 2, 3, 6, 5, 1, 2, 3, 6, 4, 2};
-    //list l(3);
-    SCD test;
-    test.executeSCD(p,3);
-    cout<<"\nFaults: "<<test.getFaults();
-    cout<<"Hits: "<<test.getHits();
-
-}
-void testFIFO(){
-   cout<<"FIFO TEST\n";
-   vector<int>p = {1, 3, 0, 3, 5, 6};
-//    {3 ,2 ,1 ,4 ,2 ,1 ,7 ,6 ,2 ,1 ,2 ,3 ,6 ,5 ,1, 2, 3 ,6 ,4 ,2};
-
-   int frames=3;
-   FIFO fifo;
-   fifo.executeFIFO(p,frames);
-   cout<<"FIFO HITS: "<<fifo.getHits()<<endl;
-   cout<<"FIFO FAULTS: "<<fifo.getFaults()<<endl<<endl;
-}
-void testLRU(){
-   cout<<"LRU TEST\n";
-   vector<int>p = {7 ,0 ,1 ,2 ,0 ,3 ,0 ,4 ,2, 3, 0, 3 ,2};
-   int frames=4;
-   LRU lru;
-   lru.executeLRU(p,frames);
-   cout<<"LRU HITS: "<<lru.getHits()<<endl;
-   cout<<"LRU FAULTS: "<<lru.getFaults()<<endl<<endl;
-}
+//void testOPT(){
+//	cout<<"OPT TEST\n";
+//	// vector<int>p={7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1};
+//    //frames=3
+//
+//	vector<int>p={7 0 1 2 0 3 0 4 2 3 0 3 2};
+//
+//	int frames=4;
+//    OPT opt;
+//    opt.executeOPT(p,frames);
+//    cout<<"OPT HITS: "<<opt.getHits()<<endl;
+//    cout<<"OPT FAULTS: "<<opt.getFaults()<<endl<<endl;
+//}
+//void testSCD(){
+//    
+//    vector<int> proc={2, 5, 10, 1, 2 ,2 ,6, 9 ,1 ,2 ,10, 2, 6, 1, 2, 1 ,6 ,9, 5, 1};
+//    //vector<int> p1={2 ,5 ,10, 1 ,2 ,2, 6, 9, 1,2, 10, 2, 6, 1 ,2 ,1, 6 ,9 ,5 ,1};
+//   vector<int> p={3, 2, 1, 4, 2, 1, 7, 6, 2, 1, 2, 3, 6, 5, 1, 2, 3, 6, 4, 2};
+//    //list l(3);
+//    SCD test;
+//    test.executeSCD(p,3);
+//    cout<<"\nFaults: "<<test.getFaults();
+//    cout<<"Hits: "<<test.getHits();
+//
+//}
+//void testFIFO(){
+//   cout<<"FIFO TEST\n";
+//   vector<int>p = {1, 3, 0, 3, 5, 6};
+////    {3 ,2 ,1 ,4 ,2 ,1 ,7 ,6 ,2 ,1 ,2 ,3 ,6 ,5 ,1, 2, 3 ,6 ,4 ,2};
+//
+//   int frames=3;
+//   FIFO fifo;
+//   fifo.executeFIFO(p,frames);
+//   cout<<"FIFO HITS: "<<fifo.getHits()<<endl;
+//   cout<<"FIFO FAULTS: "<<fifo.getFaults()<<endl<<endl;
+//}
+//void testLRU(){
+//   cout<<"LRU TEST\n";
+//   vector<int>p = {7 ,0 ,1 ,2 ,0 ,3 ,0 ,4 ,2, 3, 0, 3 ,2};
+//   int frames=4;
+//   LRU lru;
+//   lru.executeLRU(p,frames);
+//   cout<<"LRU HITS: "<<lru.getHits()<<endl;
+//   cout<<"LRU FAULTS: "<<lru.getFaults()<<endl<<endl;
+//}
